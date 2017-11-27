a05f02baa964b3918bc5d4338694c3ceb100588b
yippp <115010269@link.cuhk.edu.cn>
2017-11-27 17:57:31 +0800

    initial hw3

---
+++ hw3/src/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "console.h"
+#include "gwindow.h" // for GWindow
+#include "simpio.h"  // for getLine
+#include "vector.h"  // for Vector
+using namespace std;
+
+int main() {
+    return 0;
+}
