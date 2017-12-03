277907df2821d43571f1b8227386b1ac598c8540
yippp <115010269@link.cuhk.edu.cn>
2017-12-03 22:25:46 +0800

    finish q4, the paths are not right

--- hw3/p4.cpp
+++ hw3/p4.cpp
@@ -1,35 +1,86 @@
+/*
+ * File: p4.cpp
+ * --------------------------
+ * This is the answer to question 4 of assignmet 3 of CSC 3002 at CUHKSZ
+ * Done by Shuqian Ye, 115010269
+ */
+
 #include "graphtypes.h"
 #include "error.h"
+#include "queue.h"
+#include "stack.h"
+
 using namespace std;
+
+//Prototypes
+void initiateGraph(SimpleGraph & g);
+Node *addNode(SimpleGraph & g, string name);
+Arc *addArc(SimpleGraph & g, Node *n1, Node *n2, double cost);
+void writeGraph(const SimpleGraph & g, ostream & out);
+void printAllPathsFound(Node *n1, Node *n2, SimpleGraph & g);
+bool pathExistsBFS(Node *n1, Node *n2);
+bool pathExistsDFS(Node *n1, Node *n2);
+void findPaths(Node *destination, SimpleGraph & airline, Vector<Vector<Node *>> *paths, Vector<Node *> visited);
 
 void p4() {
     // create the graph
     SimpleGraph airline;
     initiateGraph(airline);
-    addNode(airline, "Atlanta");
-    addNode(airline, "Boston");
-    addNode(airline, "Chicago");
-    addNode(airline, "Dallas");
-    addNode(airline, "Denver");
-    addNode(airline, "Los Angeles");
-    addNode(airline, "New York");
-    addNode(airline, "Portland");
-    addNode(airline, "San Francisco");
-    addNode(airline, "Seattle");
-    addArc(airline, "Atlanta", "Chicago", 599);
-    addArc(airline, "Atlanta", "Dallas", 725);
-    addArc(airline, "Atlanta", "New York", 756);
-    addArc(airline, "Boston", "New York", 191);
-    addArc(airline, "Boston", "Seattle", 2489);
-    addArc(airline, "Chicago", "Denver", 907);
-    addArc(airline, "Dallas", "Denver", 650);
-    addArc(airline, "Dallas", "Los Angeles", 1240);
-    addArc(airline, "Dallas", "San Francisco", 1468);
-    addArc(airline, "Denver", "San Francisco", 954);
-    addArc(airline, "Portland", "San Francisco", 550);
-    addArc(airline, "Portland", "Seattle", 130);
+    Node *ATL = addNode(airline, "Atlanta");
+    Node *BOS = addNode(airline, "Boston");
+    Node *ORD = addNode(airline, "Chicago");
+    Node *DFW = addNode(airline, "Dallas");
+    Node *DEN = addNode(airline, "Denver");
+    Node *LAX = addNode(airline, "Los Angeles");
+    Node *JFK = addNode(airline, "New York");
+    Node *PDX = addNode(airline, "Portland");
+    Node *SFO = addNode(airline, "San Francisco");
+    Node *SEA = addNode(airline, "Seattle");
+    Node *HNL = addNode(airline, "Honolulu");
+
+    // both dirction
+    addArc(airline, ATL, ORD, 599);
+    addArc(airline, ATL, DFW, 725);
+    addArc(airline, ATL, JFK, 756);
+    addArc(airline, BOS, JFK, 191);
+    addArc(airline, BOS, SEA, 2489);
+    addArc(airline, ORD, DEN, 907);
+    addArc(airline, DFW, DEN, 650);
+    addArc(airline, DFW, LAX, 1240);
+    addArc(airline, DFW, SFO, 1468);
+    addArc(airline, DEN, SFO, 954);
+    addArc(airline, PDX, SFO, 550);
+    addArc(airline, PDX, SEA, 130);
+
+    addArc(airline, ORD, ATL, 599);
+    addArc(airline, DFW, ATL, 725);
+    addArc(airline, JFK, ATL, 756);
+    addArc(airline, JFK, BOS, 191);
+    addArc(airline, SEA, BOS, 2489);
+    addArc(airline, DEN, ORD, 907);
+    addArc(airline, DEN, DFW, 650);
+    addArc(airline, LAX, DFW, 1240);
+    addArc(airline, SFO, DFW, 1468);
+    addArc(airline, SFO, DEN, 954);
+    addArc(airline, SFO, PDX, 550);
+    addArc(airline, SEA, PDX, 130);
 
     writeGraph(airline, cout);
+
+    cout << endl << "path exists from Honolulu to Portland using Deep-first search: "
+         << pathExistsDFS(HNL, PDX)<< endl;
+    cout << "path exists from Honolulu to Portland using Breadth-first search: "
+         << pathExistsBFS(HNL, PDX)<< endl;
+    cout << "path exists from Denver to Portland using Deep-first search: "
+         << pathExistsDFS(DEN, PDX)<< endl;
+    cout << "path exists from Denver to Portland using Breadth-first search: "
+         << pathExistsBFS(DEN, PDX)<< endl;
+
+    cout << endl;
+    //printAllPathsFound(HNL, PDX, airline);
+    //printAllPathsFound(LAX, DFW, airline);
+    //printAllPathsFound(DFW, LAX, airline);
+    printAllPathsFound(JFK, BOS, airline);
 }
 
 /*
@@ -44,7 +95,7 @@ Node *addNode(SimpleGraph & g, string name) {
     node->name = name;
     g.nodes.add(node);
     g.nodeMap[name] = node;
-    return *node;
+    return node;
 }
 
 /*
@@ -61,7 +112,7 @@ Arc *addArc(SimpleGraph & g, Node *n1, Node *n2, double cost) {
     arc->cost = cost;
     g.arcs.add(arc);
     n1->arcs.add(arc);
-    return *arc;
+    return arc;
 }
 
 /*
@@ -105,7 +156,20 @@ void writeGraph(const SimpleGraph & g, ostream & out) {
 */
 
 bool pathExistsBFS(Node *n1, Node *n2) {
-
+    Set<Node *> visited;
+    Queue<Node *> queue;
+    queue.enqueue(n1);
+    while (!queue.isEmpty()) {
+       Node *node = queue.dequeue();
+       if (node == n2) return true;
+       if (!visited.contains(node)) {
+          visited.add(node);
+          for (Arc *arc : node->arcs) {
+             queue.enqueue(arc->finish);
+          }
+       }
+    }
+    return false;
 }
 
 /*
@@ -117,11 +181,19 @@ bool pathExistsBFS(Node *n1, Node *n2) {
 
 bool pathExistsDFS(Node *n1, Node *n2) {
     Set<Node *> visited;
-
-}
-
-bool printAllPathsFound(Node *n1, Node *n2) {
-
+    Stack<Node *> stack;
+    stack.push(n1);
+    while (!stack.isEmpty()) {
+        Node *current = stack.pop();
+        if (current == n2) return true;
+        if (!visited.contains(current)) {
+            visited.add(current);
+            for (Arc *arc : current->arcs) {
+                stack.push(arc->finish);
+            }
+        }
+    }
+    return false;
 }
 
 /*
@@ -130,3 +202,84 @@ bool printAllPathsFound(Node *n1, Node *n2) {
 * ------------------------------
 * Print all paths found from n1 to n2
 */
+
+void printAllPathsFound(Node *n1, Node *n2, SimpleGraph & airline) {
+    Vector<Vector<Node*>> paths;
+    Vector<Node *> visited;
+    visited.push_back(n1);
+    findPaths(n2, airline, &paths, visited);
+
+    // print result
+    int count = paths.size();
+    if (count == 0) {
+        cout << "There is no paths from " << n1->name
+             << " to " << n2->name << "!" << endl;
+    } else if (count > 1) {
+        cout << "There is " << count << " paths "
+             << "from " << n1->name
+             << " to " << n2->name << ":" << endl;
+        for (int i = 0; i < count; i++) {
+            cout << i+1 << ".";
+            //print path
+            Vector<Node *> path = paths.get(i);
+            for (int j = 0; j < path.size(); j++) {
+                cout << path.get(j)->name;
+                if (j < (path.size() - 1)) {
+                    cout << " -> ";
+                }
+            }
+            cout << endl;
+        }
+    } else {
+        cout << "There is 1 path " << "from " << n1->name
+             << " to " << n2->name << ":" << endl;
+        //print path
+        Vector<Node *> path = paths.get(0);
+        for (int j = 0; j < path.size(); j++) {
+            cout << path.get(j)->name;
+            if (j < (path.size() - 1)) {
+                cout << " -> ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void findPaths(Node *destination, SimpleGraph & airline, Vector<Vector<Node *>> *paths, Vector<Node *> visited) {
+    Node *current = visited.get(visited.size() - 1);
+    for (Arc *arc : (current->arcs)) {
+
+        Node *next = arc->finish;
+        bool beenVisited = false;
+        for (Node *visitedNode : visited) {
+            if (visitedNode == next) {
+                beenVisited = true;
+                continue;
+            }
+        }
+        if (!beenVisited) {
+            if (next == destination) {
+                visited.push_back(destination);
+                Vector<Node *> path = visited;
+                cout << "======" << endl;
+                paths->push_back(path);
+            } else {
+
+                visited.push_back(next);
+                //test
+//                        for (int j = 0; j < visited.size(); j++) {
+//                            cout << visited.get(j)->name;
+//                            if (j < (visited.size() - 1)) {
+//                                cout << " -> ";
+//                            }
+//                        }
+//                        cout << endl;
+
+                findPaths(destination, airline, paths, visited);
+
+            }
+        } // if the next node is not visted, continue finding path
+
+
+    }
+}
